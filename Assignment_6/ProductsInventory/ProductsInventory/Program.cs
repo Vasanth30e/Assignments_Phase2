5e4557719ab37f7ccf6725c990a13f5bbe210e19
unknown <vasanthgamer2000@gmail.com>
2023-08-18 15:54:06 +0530

    Changes have been made to Assignment and Spread Sheet

--- Assignment_6/ProductsInventory/ProductsInventory/Program.cs
+++ Assignment_6/ProductsInventory/ProductsInventory/Program.cs
@@ -13,31 +13,37 @@ namespace ProductsInventory
         static void Main(string[] args)
         {
             Produts products = new Produts();
-
-            Console.WriteLine("Enter your choice\n1. Read\n2. Insert\n3. Update" +
-                    "\n4. Delete");
-            int op = int.Parse(Console.ReadLine());
-            switch (op)
+            char choice;
+            do
             {
-                case 1:
-                    products.ReaderMethod();
-                    break;
-                case 2:
-                    products.InsertMethod();
-                    break;
-                case 3:
-                    products.UpdateMethod();
-                    break;
-                case 4:
-                    products.DeleteMethod();
-                    break;
-                default:
-                    Console.WriteLine("Wrong Choice");
-                    break;
-            }
-           Console.ReadKey();
-            
-                 
+                Console.WriteLine("Enter your choice\n1. Read\n2. Insert\n3. Update" +
+                    "\n4. Delete");
+                int op = int.Parse(Console.ReadLine());
+                
+                switch (op)
+                {
+                    case 1:
+                        products.ReaderMethod();
+                        break;
+                    case 2:
+                        products.InsertMethod();
+                        break;
+                    case 3:
+                        products.UpdateMethod();
+                        break;
+                    case 4:
+                        products.DeleteMethod();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Choice");
+                        Console.ReadKey();
+                        break;
+                }
+                Console.WriteLine("Do you wanna continue the operation\n" +
+                    "If yes press 'y' or press any key");
+                choice = char.Parse(Console.ReadLine());
+            } while (choice == 'y' || choice == 'Y');
+                                
         }
     }  
 }
