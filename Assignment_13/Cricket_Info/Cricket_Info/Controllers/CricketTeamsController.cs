75320de458834de7d767aa8bb954b98c5ec14d3a
unknown <vasanthgamer2000@gmail.com>
2023-08-29 15:44:00 +0530

    Partial View in MVC

---
+++ Assignment_13/Cricket_Info/Cricket_Info/Controllers/CricketTeamsController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Cricket_Info.Data;
+using Cricket_Info.Models;
+
+namespace Cricket_Info.Controllers
+{
+    public class CricketTeamsController : Controller
+    {
+        private CricketTeamDbContext db = new CricketTeamDbContext();
+
+        // GET: CricketTeams
+        public ActionResult Index()
+        {
+            return View(db.CricketTeams.ToList());
+        }
+
+        // GET: CricketTeams/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CricketTeam cricketTeam = db.CricketTeams.Find(id);
+            if (cricketTeam == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cricketTeam);
+        }
+
+        // GET: CricketTeams/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: CricketTeams/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Team,NOWC")] CricketTeam cricketTeam)
+        {
+            if (ModelState.IsValid)
+            {
+                db.CricketTeams.Add(cricketTeam);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(cricketTeam);
+        }
+
+        // GET: CricketTeams/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CricketTeam cricketTeam = db.CricketTeams.Find(id);
+            if (cricketTeam == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cricketTeam);
+        }
+
+        // POST: CricketTeams/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Team,NOWC")] CricketTeam cricketTeam)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cricketTeam).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cricketTeam);
+        }
+
+        // GET: CricketTeams/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            CricketTeam cricketTeam = db.CricketTeams.Find(id);
+            if (cricketTeam == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cricketTeam);
+        }
+
+        // POST: CricketTeams/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            CricketTeam cricketTeam = db.CricketTeams.Find(id);
+            db.CricketTeams.Remove(cricketTeam);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Assignment_13/Cricket_Info/Cricket_Info/Models/CricketTeam.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Cricket_Info.Models
+{
+    [Table("CricketTeam")]
+    public class CricketTeam
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Team { get; set; }
+        [Required]
+        public int NOWC { get; set; }
+    }
+}
